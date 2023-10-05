f371c8b2e1043062f3d3b8d592d94247a780e5e4
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2023-10-05 12:24:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
